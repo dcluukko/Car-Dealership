bc20347cdea7bf8090ada1f80b996913201c02a3
Student <example@example.com>
2020-02-25 10:33:35 -0800

    Added if statement that tells the user their search criteria is too low

--- Car.cs
+++ Car.cs
@@ -4,28 +4,30 @@ using System.Collections.Generic;
 public class Car
 {
   public string MakeModel;
+  public string Message;
   public int Price;
   public int Miles;
 
-  public Car(string makeModel, int price, int miles)
+  public Car(string makeModel, string message, int price, int miles)
   {
     MakeModel = makeModel;
+    Message = message;
     Price = price;
     Miles = miles;
   }
-  public bool WorthBuying(int maxPrice)
+  public bool WorthBuying(int maxPrice, int maxMiles)
   {
-    return (Price < maxPrice);
+    return (Price < maxPrice && Miles < maxMiles);
   }
 }
 public class Program
-{
+{ 
   public static void Main()
   {
-    Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792);
-    Car yugo = new Car("1980 Yugo Koral", 700, 56000);
-    Car ford = new Car("1988 Ford Country Squire", 1400, 239001);
-    Car amc = new Car("1976 AMC Pacer", 400, 198000);
+    Car volkswagen = new Car("1974 Volkswagen Thing", "Retro and cool!", 1100, 368792);
+    Car yugo = new Car("1980 Yugo Koral", "Yugo Fast!", 700, 56000);
+    Car ford = new Car("1988 Ford Country Squire", "A Classic!", 1400, 239001);
+    Car amc = new Car("1976 AMC Pacer", "Pace Yourself!", 400, 198000);
 
     List<Car> Cars = new List<Car>() { volkswagen, yugo, ford, amc };
 
@@ -33,19 +35,36 @@ public class Program
     string stringMaxPrice = Console.ReadLine();
     int maxPrice = int.Parse(stringMaxPrice);
 
-    List<Car> CarsMatchingSearch = new List<Car>(0);
+    Console.WriteLine("Enter Maximum miles: ");
+    int maxMiles = int.Parse(Console.ReadLine());
 
+    List<Car> CarsMatchingSearch = new List<Car>(0);
+    bool noMatches = true;
     foreach (Car automobile in Cars)
     {
-      if (automobile.WorthBuying(maxPrice))
+      if (automobile.WorthBuying(maxPrice, maxMiles))
       {
         CarsMatchingSearch.Add(automobile);
+        noMatches = false;
+      }
+      else 
+      {
+        noMatches = true;
       }
     }
+
+    if (noMatches) {
+      Console.WriteLine("There are no cars within your search.");
+    } 
 
     foreach (Car automobile in CarsMatchingSearch)
     {
       Console.WriteLine(automobile.MakeModel);
     }
+
+    foreach (Car automobile in CarsMatchingSearch)
+    {
+      Console.WriteLine(automobile.Message);
+    }
   }
-}
+}         
